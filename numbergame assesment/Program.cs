fd05d6c9c7cec84c5c8e6762a7ef69a2b7382c27
Sean Eastin <s198013@students.aie.edu>
2019-09-11 17:53:35 -0500

    removed fakemaxnumber and realmaxnumber since it is no longer needed

--- numbergame assesment/Program.cs
+++ numbergame assesment/Program.cs
@@ -12,21 +12,30 @@ namespace numbergame_assesment
         static void Main(string[] args)
         {
             int minnumber = 1; //min numbers starting value
-            int realmaxnumber = 101; //this is set 1 higher than the actual value because random will never reach the top value
-            int fakemaxnumber = 100; //this is what console displays
+            int maxnumber = 100; //this is set 1 higher than the actual value because random will never reach the top value
             int guess = 0;
             bool guessing = true;
             bool validanswer = false;
             string playerchoice = "0";
-            Console.WriteLine("Think of a number between " + minnumber + " and " + fakemaxnumber); //must minus one as it is accualy from 0 and 100
+            Console.WriteLine("Think of a number between " + minnumber + " and " + maxnumber); //must minus one as it is accualy from 0 and 100
             Console.WriteLine("when you are done please press a key to continue");
             Console.ReadKey();
             while (guessing)
             {
                 validanswer = false;
-                guess = random.Next(minnumber, realmaxnumber);
+                guess = random.Next(minnumber, maxnumber + 1);
                 while (!validanswer)
-                {                    
+                { 
+                    if (minnumber == maxnumber)
+                    {
+                        Console.WriteLine("computer has guessed your number this is the only possible number left " + guess);
+                        Console.ReadKey();
+                        guessing = false;
+                        validanswer = true;
+                        return;
+
+
+                    }
                     Console.WriteLine("is this your number " + guess);
                     Console.WriteLine("pick an option using numbers");
                     Console.WriteLine("1: this is the number i was thinking of");
@@ -47,7 +56,7 @@ namespace numbergame_assesment
                     }
                     else if (playerchoice == "3")
                     {
-                        realmaxnumber = guess + 1;
+                        maxnumber = guess;
                         validanswer = true;
                     }
                 }
