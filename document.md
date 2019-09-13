8bb3f3f71b416cee9c5742997062d113db131d2b
Sean Eastin <s198013@students.aie.edu>
2019-09-13 12:36:09 -0500

    new things added to finish assesment

---
+++ document.md
@@ -0,0 +1,10 @@
+|Sean Eastin|
+| :---      |
+|s198013|
+|number game mock assesment|
+|number game mock assesment|
+| ![User Interface gif](numberguessinggameoutline.jpg)
+
+## requirements
+
+the program will ask the player to pick a number and then try to guess the number
--- numbergame assesment/Program.cs
+++ numbergame assesment/Program.cs
@@ -8,12 +8,14 @@ namespace numbergame_assesment
 {
     class Program
     {
+        
         public static Random random = new Random();
         static void Main(string[] args)
         {
             int minnumber = 1; //min numbers starting value
             int maxnumber = 100; //this is set 1 higher than the actual value because random will never reach the top value
             int guess = 0;
+            int loopstook = 0; //usses to tell how many loops was used
             bool guessing = true;
             bool validanswer = false;
             string playerchoice = "0";
@@ -26,7 +28,7 @@ namespace numbergame_assesment
                 guess = random.Next(minnumber, maxnumber + 1);
                 while (!validanswer)
                 { 
-                    if (minnumber == maxnumber)
+                    if (minnumber == maxnumber) //this should do if thier is only 1 possible number
                     {
                         Console.WriteLine("computer has guessed your number this is the only possible number left " + guess);
                         Console.ReadKey();
@@ -42,12 +44,14 @@ namespace numbergame_assesment
                     Console.WriteLine("2: the number is greater than the guess");
                     Console.WriteLine("3: the number is less than the guess");
                     playerchoice = Console.ReadLine();
+
                     if (playerchoice == "1")
                     {
                         Console.WriteLine("computer has guessed your number");
                         Console.ReadKey();
                         guessing = false;
                         validanswer = true;
+                        
                     }
                     else if (playerchoice == "2")
                     {
